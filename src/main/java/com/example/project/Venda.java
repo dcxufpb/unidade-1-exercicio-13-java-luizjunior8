17a2cd53bcd94d9263a60794657ae895767ef3e4
luizjunior8 <luiz.oliveira@dcx.ufpb.br>
2020-10-12 20:23:13 -0300

    Mission Accomplished

--- src/main/java/com/example/project/Venda.java
+++ src/main/java/com/example/project/Venda.java
@@ -1,5 +1,88 @@
 package com.example.project;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
+ 
 public class Venda {
     
+    private Loja loja;
+    private String dataHora;
+    private int ccf;
+    private int coo;
+
+    public Venda(Loja loja, String dataHora, int ccf, int coo) {
+        this.loja = loja;
+        this.ccf = ccf;
+        this.coo = coo;
+        this.dataHora = getSystemData();
+    }
+
+	public Loja getLoja() {
+		return this.loja;
+	}
+
+	public void setLoja(Loja loja) {
+		this.loja = loja;
+	}
+
+	public int getCcf() {
+		return this.ccf;
+	}
+
+	public void setCcf(int ccf) {
+		this.ccf = ccf;
+	}
+
+	public int getCoo() {
+		return this.coo;
+	}
+
+	public void setCoo(int coo) {
+		this.coo = coo;
+	}
+
+	public String getdataHora() {
+		return this.dataHora;
+	}
+
+	public void setDataHora(String dataHora) {
+		this.dataHora = dataHora;
+    }
+        
+    
+    public static String getSystemData() {
+        return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+    }
+
+  
+    public void validar_dados_obrigatorios() {
+
+        if (this.loja == null){
+            throw new RuntimeException ("Informe uma loja válida");
+        }
+
+        if (this.ccf == 0){
+            throw new RuntimeException ("O campo ccf é obrigatório");
+        }
+
+        if (this.coo == 0){
+            throw new RuntimeException ("O campo coo é obrigatório");
+        }
+
+        if (this.dataHora == null || this.dataHora == "") {
+            throw new RuntimeException ("A data e a hora são obrigatórias");
+        }
+    }
+
+    public String dadosVenda() {
+
+        this.validar_dados_obrigatorios();
+
+        String _ccf = " CCF:" + this.ccf;
+
+        String _coo = " COO:" + this.coo;
+
+        return (this.getdataHora() + _ccf + _coo);
+
+       
+    }
 }
--- src/test/java/com/example/project/TestVenda.java
+++ src/test/java/com/example/project/TestVenda.java
@@ -5,8 +5,56 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class TestVenda {
-    	@Test
-	public void validarX() {
-        fail();
+
+    private void verificarCampoObrigatorio(String mensagemEsperada, Venda venda) {
+		try {
+			venda.dadosVenda();
+		} catch (RuntimeException e) {
+			assertEquals(mensagemEsperada, e.getMessage());
+		}
+    }
+
+    private String NOME_LOJA = "Loja 1";
+	private String LOGRADOURO = "Log 1";
+	private int NUMERO = 10;
+	private String COMPLEMENTO = "C1";
+	private String BAIRRO = "Bai 1";
+	private String MUNICIPIO = "Mun 1";
+	private String ESTADO = "E1";
+	private String CEP = "11111-111";
+	private String TELEFONE = "(11) 1111-1111";
+	private String OBSERVACAO = "Obs 1";
+	private String CNPJ = "11.111.111/1111-11";
+    private String INSCRICAO_ESTADUAL = "123456789";
+    private String DATA_HORA = Venda.getSystemData();
+    private int CCF = 123456;
+    private int COO = 654321;
+    
+    private Endereco brickEndereco = new Endereco(LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO, ESTADO, CEP);
+    
+    private Loja brickLoja = new Loja (NOME_LOJA, brickEndereco, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
+
+    	
+	public void validLoja() {
+        Venda lojaNull = new Venda (null, DATA_HORA, CCF, COO);
+		verificarCampoObrigatorio("Informe uma loja válida", lojaNull);
+    }
+
+    public void validTime() {
+        Venda nullTime = new Venda (brickLoja, null, CCF, COO);
+        verificarCampoObrigatorio("A data e a hora são obrigatórias", nullTime);
+
+        Venda emptyTime = new Venda (brickLoja, "", CCF, COO);
+        verificarCampoObrigatorio("A data e a hora são obrigatórias", emptyTime);
+    }
+
+    public void validCCF() {
+        Venda nullCcf = new Venda(brickLoja, DATA_HORA, 0, COO);
+        verificarCampoObrigatorio("O campo ccf é obrigatório", nullCcf);
+    }
+
+    public void validCOO() {
+        Venda nullCoo = new Venda(brickLoja, DATA_HORA, CCF, 0);
+        verificarCampoObrigatorio("O campo ccf é obrigatório", nullCoo);
     }
 }
